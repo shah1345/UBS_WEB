6ca372e972c60f8ec880a61123e8c802c07f9cd6
Shah Newaz <34304584+shah1345@users.noreply.github.com>
2022-04-19 12:50:17 +0600

    Add files via upload

---
+++ ubs_db.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2022 at 08:49 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ubs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus_location`
+--
+-- Error reading structure for table ubs_db.bus_location: #1932 - Table 'ubs_db.bus_location' doesn't exist in engine
+-- Error reading data for table ubs_db.bus_location: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ubs_db`.`bus_location`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus_sit_booking`
+--
+-- Error reading structure for table ubs_db.bus_sit_booking: #1932 - Table 'ubs_db.bus_sit_booking' doesn't exist in engine
+-- Error reading data for table ubs_db.bus_sit_booking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ubs_db`.`bus_sit_booking`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `otp_config`
+--
+-- Error reading structure for table ubs_db.otp_config: #1932 - Table 'ubs_db.otp_config' doesn't exist in engine
+-- Error reading data for table ubs_db.otp_config: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ubs_db`.`otp_config`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_data`
+--
+-- Error reading structure for table ubs_db.user_data: #1932 - Table 'ubs_db.user_data' doesn't exist in engine
+-- Error reading data for table ubs_db.user_data: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ubs_db`.`user_data`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
